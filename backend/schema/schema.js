957ce6e80dfcbef5ab0ae59e59a1bb4ffaf04e31
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-11-16 13:39:39 +0000

    route can be teasted

---
+++ backend/schema/schema.js
@@ -0,0 +1,63 @@
+import graphql from "graphql";
+import _ from "lodash";
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+const recipes = [
+  {
+    name: "Spaghetti Aglio Olio e Peperoncino",
+    category: "Pasta",
+    ingredients: "linguine, Onion, Chillies, Garlic",
+    instructions: "Boil Water",
+    id: "1",
+  },
+  {
+    name: "Bolognese ragù with pappardelle",
+    category: "Pasta",
+    ingredients: "Paperelle",
+    instructions: "Boil Water",
+    id: "2",
+  },
+  {
+    name: "Spaghetti Aglio Olio e Peperoncino",
+    category: "Pasta",
+    ingredients: "linguine, Onion, Chillies, Garlic",
+    instructions: "Boil Water",
+    id: "3",
+  },
+  {
+    name: "Pork meatballs with anchovies",
+    category: "Mediterranean",
+    ingredients: "Pork",
+    instructions: "Yum yummy",
+    id: "4",
+  },
+];
+
+const RecipeType = new GraphQLObjectType({
+  name: "Recipe",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    category: { type: GraphQLString },
+    ingredients: { type: GraphQLString },
+    instructions: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: () => ({
+    recipe: {
+      type: RecipeType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        return _.find(recipes, { id: args.id });
+      },
+    },
+  }),
+});
+
+export default new GraphQLSchema({
+  query: RootQuery,
+});
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,34 @@
 import express from "express";
 import dotenv from "dotenv";
+import { graphqlHTTP } from "express-graphql";
+import schema from "./schema/schema.js";
+
 import recipes from "./data/recipes.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 
-app.get("/api/recipes", (req, res) => {
-  res.json(recipes);
-});
+// app.get("/", (req, res) => {
+//   res.send("API is running...");
+// });
 
-app.get("/api/recipes/:id", (req, res) => {
-  const recipe = recipes.find((r) => r._id === req.params.id);
-  res.json(recipe);
-});
+// app.get("/api/recipes", (req, res) => {
+//   res.json(recipes);
+// });
+
+// app.get("/api/recipes/:id", (req, res) => {
+//   const recipe = recipes.find((r) => r._id === req.params.id);
+//   res.json(recipe);
+// });
 
 const PORT = process.env.PORT || 5000;
 
