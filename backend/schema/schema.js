a745ae6b736198e8b9629c995031720d63d4fa98
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-11-17 10:35:37 +0000

    added recipe mutation

--- backend/schema/schema.js
+++ backend/schema/schema.js
@@ -1,5 +1,7 @@
 import graphql from "graphql";
 import _ from "lodash";
+import Recipe from "../models/recipe.js";
+import Main from "../models/main.js";
 
 const {
   GraphQLObjectType,
@@ -8,112 +10,6 @@ const {
   GraphQLID,
   GraphQLList,
 } = graphql;
-
-const recipes = [
-  {
-    name: "Spaghetti Aglio Olio e Peperoncino",
-    category: "Pasta",
-    ingredients: "linguine, Onion, Chillies, Garlic",
-    instructions: "Boil Water",
-    id: "1",
-    mainId: "1",
-  },
-  {
-    name: "Bolognese ragù with pappardelle",
-    category: "Pasta",
-    ingredients: "Paperelle",
-    instructions: "Boil Water",
-    id: "2",
-    mainId: "2",
-  },
-  {
-    name: "Pork meatballs with anchovies",
-    category: "Mediterranean",
-    ingredients: "Pork",
-    instructions: "Yum yummy",
-    id: "3",
-    mainId: "3",
-  },
-  {
-    name: "Taiwanese Seabass",
-    category: "Asian",
-    ingredients: "Seabass",
-    instructions: "Yum yummy",
-    id: "4",
-    mainId: "4",
-  },
-  {
-    name: "Chicken with Lemon",
-    category: "Mediterranean",
-    ingredients: "garlic",
-    instructions: "Yum yummy",
-    id: "5",
-    mainId: "5",
-  },
-  {
-    name: "Roat Chicken",
-    category: "Mediterranean",
-    ingredients: "chillies, rosemary",
-    instructions: "Yum yummy",
-    id: "6",
-    mainId: "5",
-  },
-  {
-    name: "Roast Seabass",
-    category: "Asian",
-    ingredients: "ginger",
-    instructions: "Yum yummy",
-    id: "7",
-    mainId: "4",
-  },
-  {
-    name: "Roast Pork",
-    category: "Mediterranean",
-    ingredients: "Carrots",
-    instructions: "Yum yummy",
-    id: "8",
-    mainId: "3",
-  },
-  {
-    name: "Roast Beef",
-    category: "Mediterranean",
-    ingredients: "Olive oil",
-    instructions: "Yum yummy",
-    id: "9",
-    mainId: "2",
-  },
-  {
-    name: "Roast Vegetables",
-    category: "Mediterranean",
-    ingredients: "thyme",
-    instructions: "Yum yummy",
-    id: "10",
-    mainId: "1",
-  },
-];
-
-const mains = [
-  {
-    type: "Vegetarian",
-    id: "1",
-  },
-  {
-    type: "Beef",
-    id: "2",
-  },
-  {
-    type: "Pork",
-    id: "3",
-  },
-  {
-    type: "Fish",
-    id: "4",
-  },
-  {
-    type: "Chicken",
-    id: "5",
-  },
-];
 
 const RecipeType = new GraphQLObjectType({
   name: "Recipe",
@@ -125,9 +21,7 @@ const RecipeType = new GraphQLObjectType({
     instructions: { type: GraphQLString },
     main: {
       type: MainType,
-      resolve(parent, type) {
-        return _.find(mains, { id: parent.mainId });
-      },
+      resolve(parent, type) {},
     },
   }),
 });
@@ -139,9 +33,7 @@ const MainType = new GraphQLObjectType({
     type: { type: GraphQLString },
     recipe: {
       type: new GraphQLList(RecipeType),
-      resolve(parent, args) {
-        return _.filter(recipes, { mainId: parent.id });
-      },
+      resolve(parent, args) {},
     },
   }),
 });
@@ -152,16 +44,12 @@ const RootQuery = new GraphQLObjectType({
     recipe: {
       type: RecipeType,
       args: { id: { type: GraphQLID } },
-      resolve(parent, args) {
-        return _.find(recipes, { id: args.id });
-      },
+      resolve(parent, args) {},
     },
     main: {
       type: MainType,
       args: { id: { type: GraphQLID } },
-      resolve(parent, args) {
-        return _.find(mains, { id: args.id });
-      },
+      resolve(parent, args) {},
     },
     recipe: {
       type: new GraphQLList(RecipeType),
@@ -177,17 +65,28 @@ const RootQuery = new GraphQLObjectType({
     },
     recipes: {
       type: new GraphQLList(RecipeType),
-      resolve(parent, args) {
-        return Recipe.find({});
-      },
+      resolve(parent, args) {},
     },
     mains: {
       type: new GraphQLList(MainType),
-      resolve(parent, args) {
-        return Main.find({});
-      },
+      resolve(parent, args) {},
     },
   }),
+});
+
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addRecipe: {
+      type: RecipeType,
+      args: {
+        name: { type: GraphQLString },
+        category: { type: GraphQLString },
+        ingredients: { type: GraphQLString },
+        instructions: { type: GraphQLString },
+      },
+    },
+  },
 });
 
 export default new GraphQLSchema({
